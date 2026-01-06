08d45852b2b4f42ca404c17b9262c1813165bc70
Kakarot-Caracter <itachimartinez0@gmail.com>
2026-01-06 17:08:09 -0300

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -11,7 +11,6 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { useAuthStore } from "../stores/auth.store";
 
 export default function Header() {
   const isAuthenticated = false;
