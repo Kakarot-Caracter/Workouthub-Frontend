882e2fc79fa6a39f9e72ac489638b5be086a3c54
Kakarot-Caracter <itachimartinez0@gmail.com>
2026-01-07 02:41:23 -0300

    Update ProfileSectionClient.tsx

--- app/perfil/components/wrappers/ProfileSectionClient.tsx
+++ app/perfil/components/wrappers/ProfileSectionClient.tsx
@@ -3,11 +3,14 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/lib/queryClient";
 import ProfileSection from "../ProfileSection";
+import ProtectedRoute from "@/lib/ProtectedRoute";
 
 export function ProfileSectionClient() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ProfileSection />
+      <ProtectedRoute>
+        <ProfileSection />
+      </ProtectedRoute>
     </QueryClientProvider>
   );
 }
